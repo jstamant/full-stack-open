96b402d55eaf89b9d221cb5ac112dc2f8bc18372
Justin R. St-Amant <jstamant24@gmail.com>
2024-02-23 11:24:47 -0600

    Added submission for exercise 1.9

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -6,15 +6,24 @@ const Statistics = ({good, neutral, bad}) => {
   const average = (good - bad) / total
   const positive = good / total * 100
 
+  if (total > 0) {
+    return (
+      <>
+        <h1>Statistics</h1>
+        <p>Good: {good}</p>
+        <p>Neutral: {neutral}</p>
+        <p>Bad: {bad}</p>
+        <p>Total: {total}</p>
+        <p>Average: {average}</p>
+        <p>Positive: {positive}%</p>
+      </>
+    )
+  }
+
   return (
     <>
       <h1>Statistics</h1>
-      <p>Good: {good}</p>
-      <p>Neutral: {neutral}</p>
-      <p>Bad: {bad}</p>
-      <p>Total: {total}</p>
-      <p>Average: {average}</p>
-      <p>Positive: {positive}%</p>
+      <p>No feedback given</p>
     </>
   )
 }
