2cfb785a63990623d5a106fdb79edf773aed43c3
Justin R. St-Amant <jstamant24@gmail.com>
2024-03-05 16:18:07 -0600

    Added submission for exercise 4.5

--- part4/blog-list/utils/list_helper.js
+++ part4/blog-list/utils/list_helper.js
@@ -2,11 +2,16 @@ const dummy = (blogs) => {
   return 1
 }
 
+const favoriteBlog = (posts) => {
+  return posts.toSorted((a, b) => b.likes - a.likes)[0]
+}
+
 const totalLikes = (posts) => {
   return posts.reduce((acc, post) => acc + post.likes, 0)
 }
 
 module.exports = {
   dummy,
+  favoriteBlog,
   totalLikes
 }
